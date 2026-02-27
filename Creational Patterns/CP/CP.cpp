97e196e5f56031334b9669de2e4c102087b424ff
cloud <cloudtail4571@gmail.com>
2026-02-27 19:24:20 +0500

    Написала комментарии для SP и CP

--- Creational Patterns/CP/CP.cpp
+++ Creational Patterns/CP/CP.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+/// \brief Интерфейс для персонажей
 class Character {
     int health;
 public:
@@ -13,37 +14,102 @@ public:
     }
 };
 
+/// \brief Класс с настройками для персонажа "Воин"
 class Warrior : public Character {
 public: 
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return string - строку, название персонажа
+    * \remark Получаем только имя персонажа т.к это учебный код
+    */
     string getType() override { return "Воин"; }
+    /**
+    * \brief Функция для выполнения действий персонажа
+    * \note Не влияет ни на что. Это учебный код
+    * \todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Воин атакует мечом!" << endl; }
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return int - число, количество здоровья персонажа
+    * \note Не влияет ни на что. Это учебный код
+    */
     int getHealth() override { return 100; }
 };
 
+/// \brief Класс с настройками для персонажа "Маг"
 class Mage : public Character {
 public:
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return string - строку, название персонажа
+    * \remark Получаем только имя персонажа т.к это учебный код 
+    */
     string getType() override { return "Маг"; }
+    /**
+    * \brief Функция для выполнения действий персонажа
+    * \note Не влияет ни на что. Это учебный код
+    * \todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Маг кастует огненный шар!" << endl; }
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return int - число, количество здоровья персонажа
+    * \note Не влияет ни на что. Это учебный код
+    */
     int getHealth() override { return 50; }
 };
 
+/// \brief Класс с настройками для персонажа "Лучник"
 class Archer : public Character {
 public:
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return string - строку, название персонажа
+    * \remark Получаем только имя персонажа т.к это учебный код
+    */
     string getType() override { return "Лучник"; }
+    /**
+    * \brief Функция для выполнения действий персонажа
+    * \note Не влияет ни на что. Это учебный код
+    * \todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Лучник стреляет из лука!" << endl; }
     int getHealth() override { return 100; }
 };
 
+/// \brief Класс с настройками для персонажа "Рыцарь"
 class Knight : public Character {
 public:
+    /**
+    * \brief Функция для получения типа персонажа
+    * \return string - строку, название персонажа
+    * \remark Получаем только имя персонажа т.к это учебный код
+    */
     string getType() override { return "Рыцарь"; }
+    /**
+    * \brief Функция для выполнения действий персонажа
+    * \note Не влияет ни на что. Это учебный код
+    * \todo Добавить возможность отбирать жизни у того, на кого нападаем
+    */
     void attack() override { cout << "Рыцарь атакует копьем!" << endl; }
     int getHealth() override { return 70; }
 };
 
+/**
+* \brief Абстрактная фабрика для генерации персонажей
+* 
+* \Используется для примера работы паттерна проектирования 
+*/
+
 class CharacterFactory{
 public:
     virtual Character* createCharacter() = 0;
+    /**
+    * \brief Функция для создания персонажей и выполнения базовых его действий
+    * \note После создания объекта, выполняет действие персонажа и уничтожает объект
+    * \warning Нельзя будет использовать созданных персонажей в других частях кода
+    */
     void spawnAndAttack() {
         Character* chara = createCharacter();
         cout << "Создан " << chara->getType() << ". ";
@@ -55,26 +121,46 @@ public:
 
 };
 
+/// \brief Класс для создания Воина
 class WarriorFactory : public CharacterFactory {
 public:
+    /**
+    * \brief Функция для выделения памяти на класс персонажа
+    * \return Возвращает указатель на созданного персонажа
+    */
     Character* createCharacter() override { return new Warrior(); }
 };
 
 class MageFactory : public CharacterFactory {
 public:
+    /**
+    * \brief Функция для выделения памяти на класс персонажа
+    * \return Возвращает указатель на созданного персонажа
+    */
     Character* createCharacter() override { return new Mage(); }
 };
 
 class ArcherFactory : public CharacterFactory {
 public:
+    /**
+    * \brief Функция для выделения памяти на класс персонажа
+    * \return Возвращает указатель на созданного персонажа
+    */
     Character* createCharacter() override { return new Archer(); }
 };
 
 class KnightFactory : public CharacterFactory {
 public:
+    /**
+    * \brief Функция для выделения памяти на класс персонажа
+    * \return Возвращает указатель на созданного персонажа
+    */
     Character* createCharacter() override { return new Knight(); }
 };
 
+
+/// \brief Функция для запуска конвеера с персонажами
+/// \note Используется для учебного кода
 int main()
 {
     setlocale(LC_ALL, "RUS");
--- Structural Patterns/SP/SP.cpp
+++ Structural Patterns/SP/SP.cpp
@@ -2,75 +2,140 @@
 #include <string>
 using namespace std;
 
+/// \brief Класс-шаблон для напитков
 class Beverage {
 public:
     virtual string getDescription() = 0;
     virtual double cost() = 0;
 };
 
+/// \brief Класс напитка "Эспрессо"
 class Espresso : public Beverage {
 public:
+    /**
+    * \brief Функция для получения названия напитка
+    * \return string - строку, название напитка
+    */
     string getDescription() override {
         return "Эспрессо";
     }
+    /**
+    * \brief Функция для получения цены напитка
+    * \return double - возвращение цены напитка
+    */
     double cost() override { return 1.99; }
 };
 
+/// \brief Класс напитка "Чай"
 class Tea : public Beverage {
 public:
+    /**
+    * \brief Функция для получения названия напитка
+    * \return string - строку, название напитка
+    */
     string getDescription() override {
         return "Чай";
     }
+    /**
+    * \brief Функция для получения цены напитка
+    * \return double - возвращение цены напитка
+    */
     double cost() override { return 1.50; }
 };
 
+/// \brief Класс-шаблон для добавок
 class CondimentDecorator : public Beverage {
 protected:
     Beverage* bev;
 public:
+    /**
+    * \brief Функция для получения названия добавки
+    * \return bev - получение названия добавки
+    */
     CondimentDecorator(Beverage* b) : bev(b) {}
     string getDescription() override {
         return bev->getDescription();
     }
+    /**
+    * \brief Функция для определения цены добавки
+    * \return bev - получение цены добавки
+    */
     double cost() override { return bev->cost(); }
 };
 
+/// \brief Класс для добавки "Молоко"
 class Milc : public CondimentDecorator {
 public:
+    /**
+    * \brief Функция для получения названия добавки
+    * \return bev - получение названия добавки
+    */
     Milc(Beverage* b) : CondimentDecorator(b) {}
     string getDescription() override {
         return bev->getDescription() + ", Молоко";
     }
+    /**
+    * \brief Функция для получения цены добавки
+    * \return bev - получение цены добавки
+    */
     double cost() override { return bev->cost() + 0.10; }
 };
 
+/// \brief Класс для добавки "Сахар"
 class Sugar : public CondimentDecorator {
 public:
+    /**
+    * \brief Функция для получения названия добавки
+    * \return bev - получение названия добавки
+    */
     Sugar(Beverage* b) : CondimentDecorator(b) {}
     string getDescription() override {
         return bev->getDescription() + ", Сахар";
     }
+    /**
+    * \brief Функция для получения цены добавки
+    * \return bev - получение цены добавки
+    */
     double cost() override { return bev->cost() + 0.05; }
 };
 
+/// \brief Класс для добавки "Сироп"
 class Syrop : public CondimentDecorator {
 public:
+    /**
+    * \brief Функция для получения названия добавки
+    * \return bev - получение названия добавки
+    */
     Syrop(Beverage* b) : CondimentDecorator(b) {}
     string getDescription() override {
         return bev->getDescription() + ", Сироп";
     }
+    /**
+    * \brief Функция для получения цены добавки
+    * \return bev - получение цены добавки
+    */
     double cost() override { return bev->cost() + 0.15; }
 };
 
+/// \brief Класс для добавки "Шоколад"
 class Choco : public CondimentDecorator {
 public:
+    /**
+    * \brief Функция для получения названия добавки
+    * \return bev - получение названия добавки
+    */
     Choco(Beverage* b) : CondimentDecorator(b) {}
     string getDescription() override {
         return bev->getDescription() + ", Шоколад";
     }
+    /**
+    * \brief Функция для получения цены добавки
+    * \return bev - получение цены добавки
+    */
     double cost() override { return bev->cost() + 0.20; }
 };
 
+/// \brief Функция для запуска тестовых чеков с использованием комбинации разных напитков
 int main()
 {
     setlocale(LC_ALL, "RUS");
